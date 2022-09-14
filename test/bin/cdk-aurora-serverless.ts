4a8cca812149da3abb8c816969d753c008386da9
linhtruong8 <111520326+linhtruong8@users.noreply.github.com>
2022-09-14 16:31:32 -0400

    delete bin folder

--- test/bin/cdk-aurora-serverless.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkAuroraServerlessStack } from '../enhanced-leaderboard-view-backend/testing/lib/cdk-aurora-serverless-stack';
-
-const app = new cdk.App();
-new CdkAuroraServerlessStack(app, 'CdkAuroraServerlessStack');
